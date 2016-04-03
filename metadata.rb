d6e0e27d06f6b5dec31bf78214c9a19c2e0c583c
lebedik <evgeny.lebedik@gmail.com>
2016-04-03 12:16:10 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
-maintainer "Yauheni Lebedzik"
-maintainer_email "yauheni.lebedzik@sap.com"
+name 'zabbix'
+maintainer "lebedik"
+maintainer_email "evgeny.lebedik@gmail.com"
 description "installs and configure yard-chef"
 version "1.0.0"
 
